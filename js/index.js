24a48f7050596a3542f3ffc9a56cc104e05b7302
MGRookie <574169815@qq.com>
2020-07-07 20:38:19 +0800

    jjj

--- js/index.js
+++ js/index.js
@@ -9,27 +9,10 @@
 
 
 
-  // oUl.addEventListener('touchstart', function (e) {
-  //   let target = e.target;
-
+  oUl.addEventListener('touchstart', function (e) {
+    let target = e.target;
+    console.log(target);
     
-  //   if(target.nodeName =="IMG"){
-  //     target = target.parentElement;
-  //     console.log(target);
-  //   }
-
-  //   if (target.classList.contains('select')) {
-  //     target.classList.remove('select');
-  //   } else {
-  //     target.classList.add('select');
-  //   }
-
-  // }, false)
-
-  oUl.ontouchstart = function (e) {
-    let event = e || window.event;
-    let target = event.target || event.srcElement;
-  
     if(target.nodeName =="IMG"){
       target = target.parentElement;
       console.log(target);
@@ -41,12 +24,30 @@
       target.classList.add('select');
     }
 
-  }
+  }, false)
+  // oUl.ontouchstart = function (e) {
+    
+  //   let event = e || window.event;
+  //   let target = event.target || event.srcElement;
+  // console.log('aaa');
+  //   if(target.nodeName =="IMG"){
+  //     target = target.parentElement;
+  //     console.log(target);
+  //   }
+
+  //   if (target.classList.contains('select')) {
+  //     target.classList.remove('select');
+  //   } else {
+  //     target.classList.add('select');
+  //   }
+
+  // }
 
 
   const oT = document.querySelector('.test');
 
   oT.addEventListener('touchstart',function(e){
+    console.log(this);
     this.style.backgroundColor = 'red';
   },false)
 
