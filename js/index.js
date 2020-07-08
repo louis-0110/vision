e7f9203c81ec99ffe08a669de7c97525cf7c634d
MGRookie <574169815@qq.com>
2020-07-08 09:10:45 +0800

    pp

--- js/index.js
+++ js/index.js
@@ -6,16 +6,11 @@
 
   const oUl = document.querySelector('ul');
 
-
-
-
   oUl.addEventListener('touchstart', function (e) {
     let target = e.target;
-    console.log(target);
-    
-    if(target.nodeName =="IMG"){
+    console.log('aa');
+    if (target.nodeName == "IMG") {
       target = target.parentElement;
-      console.log(target);
     }
 
     if (target.classList.contains('select')) {
@@ -25,30 +20,5 @@
     }
 
   }, false)
-  // oUl.ontouchstart = function (e) {
-    
-  //   let event = e || window.event;
-  //   let target = event.target || event.srcElement;
-  // console.log('aaa');
-  //   if(target.nodeName =="IMG"){
-  //     target = target.parentElement;
-  //     console.log(target);
-  //   }
-
-  //   if (target.classList.contains('select')) {
-  //     target.classList.remove('select');
-  //   } else {
-  //     target.classList.add('select');
-  //   }
-
-  // }
-
-
-  const oT = document.querySelector('.test');
-
-  oT.addEventListener('touchstart',function(e){
-    console.log(this);
-    this.style.backgroundColor = 'red';
-  },false)
 
 }())
