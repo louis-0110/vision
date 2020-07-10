712c02aef31caadf6410335ec011d44a748fec7c
MGRookie <574169815@qq.com>
2020-07-10 16:55:10 +0800

    666

--- js/index.js
+++ js/index.js
@@ -276,6 +276,6 @@ UpTime.prototype.render = function () {
 
 
 
-const uptime = new UpTime(12);
+const uptime = new UpTime(120);
 
 
--- js/popups.js
+++ js/popups.js
@@ -5,34 +5,39 @@
 //1.倒计时结束 弹出 很遗憾！再接再厉！
 //2.完成时，弹出 恭喜🎉完成
 
-function Popups(){
+function Popups() {
   this.dom = document.querySelector('.popups');
+  this.closeBtn = document.querySelector('.close');
+  this.content = document.querySelector('.pContent');
   this.status = 'none';
   this.dom.addEventListener('touchstart', ev => {
     ev.preventDefault();
   })
+
+  this.closeBtn.addEventListener('touchstart',()=>{
+    this.none();
+  })
+
 }
 
-Popups.prototype.none= function(){
+Popups.prototype.none = function () {
   this.dom.classList.add('none');
   this.status = 'none';
 }
 
-Popups.prototype.block = function(){
+Popups.prototype.block = function () {
   this.dom.classList.remove('none');
   this.status = 'block';
 }
 
 
-Popups.prototype.setContent = function(flag){
-  if(flag == 1){
-    this.dom.innerHTML = `<p>恭喜🎉过关</p>`;
-  }else if (flag == 0){
-    this.dom.innerHTML = `<p>遗憾！再接再厉！</p>`;
+Popups.prototype.setContent = function (flag) {
+  if (flag == 1) {
+    this.content.innerHTML = `<p style='font-size:26px;color:#fff;text-alidn=center'>恭喜🎉过关</p>`;
+  } else if (flag == 0) {
+    this.content.innerHTML = `<p style='font-size:26px;color:#fff;text-alidn=center'>遗憾！再接再厉！</p>`;
   }
 
-
-  
 }
 
 const pop = new Popups();
@@ -53,11 +58,11 @@ const pop = new Popups();
 //     ev.preventDefault();
 //   })
 
-  
+
 
 
 //   oUl.addEventListener('touchstart', function (e) {
-    
+
 //     let target = e.target;
 //     if (target.nodeName == "IMG") {
 //       target = target.parentElement;
